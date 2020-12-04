130c08c5f448d36fda9fd5b96f710ed6831694c0
iamHolo <nneketaa@yandex.ru>
2020-12-04 15:59:41 +0500

    Добавьте файлы проекта.

---
+++ Triangles/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangles
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Triangle trg1 = new Triangle(7, 7, 7);
+            Triangle trg2 = new Triangle(7, 6, 3);
+            Triangle trg3 = new Triangle(5, 5, 3);
+            Triangle trg4 = new Triangle(4, 5, 3);
+            Triangle trg5 = new Triangle(8, 8, 8);
+            Triangle trg6 = new Triangle(9, 53, 2);
+
+            Console.WriteLine("Равносторонние треугольники : \n");
+            foreach(string trg in Triangle.equalTrgs)
+            {
+                Console.WriteLine(trg+"\n");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Равнобедренные треугольники : \n");
+            foreach (string trg in Triangle.esoscleTrgs)
+            {
+                Console.WriteLine(trg + "\n");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Прямоугольные треугольники : \n");
+            foreach (string trg in Triangle.rectangleTrgs)
+            {
+                Console.WriteLine(trg + "\n");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Произвольные треугольники : \n");
+            foreach (string trg in Triangle.randomTrgs)
+            {
+                Console.WriteLine(trg + "\n");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Треугольники, которые нельзя построить : \n");
+            foreach (string trg in Triangle.notTrgs)
+            {
+                Console.WriteLine(trg + "\n");
+            }
+            Console.WriteLine();
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Triangles/Triangles.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+
+namespace Triangles
+{
+    class Triangle
+    {
+        private bool triangle;
+        private double square;
+        private double perimeter;
+
+        public static List<string> equalTrgs = new List<string>();
+        public static List<string> esoscleTrgs = new List<string>();
+        public static List<string> rectangleTrgs = new List<string>();
+        public static List<string> randomTrgs = new List<string>();
+        public static List<string> notTrgs = new List<string>();
+
+        public Triangle(int l1, int l2, int l3)
+        {
+            double perimeter_ = l1 + l2 + l3;
+            double halfOfP = perimeter / 2;
+            double square_ = Math.Sqrt(halfOfP*(halfOfP - l1)*(halfOfP - l2)*(halfOfP - l3));
+
+            if ((l1 + l2 > l3) && (l2 + l3 > l1) && (l1 + l3 > l2))
+            {
+                triangle = true;
+                square = square_;
+                perimeter = perimeter_;
+
+                if (l1 == l2 && l1 == l3) //Равносторонний
+                {
+                   equalTrgs.Add($"Треугольник a = {l1} b = {l2} c = {l3}");
+                }
+                else if ((l1 == l2 && l1 != l3) || (l2 == l3 && l2 != l1) || (l3 == l1 && l3 != l2)) //Равнобедренный
+                {
+                   esoscleTrgs.Add($"Треугольник a = {l1} b = {l2} c = {l3}");
+                }
+                else if ((l1 * l1 + l2 * l2 == l3 * l3) || (l1 * l1 + l3 * l3 == l2 * l2) || (l3 * l3 + l2 * l2 == l1 * l1)) //Прямоугольный
+                {
+                   rectangleTrgs.Add($"Треугольник a = {l1} b = {l2} c = {l3}");
+                }
+                else  //Произвольный
+                {
+                   randomTrgs.Add($"Треугольник a = {l1} b = {l2} c = {l3}");
+                }
+
+            }
+            else
+            {
+                //Нельзя построить
+                notTrgs.Add($"Треугольник a = {l1} b = {l2} c = {l3}");
+                triangle = false;
+            }
+        }
+    }
+}
